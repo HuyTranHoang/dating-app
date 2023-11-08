5af4ec74235f7fb611114f539956a42dd4b86b94
Huy Tran Hoang <nhokpio95@gmail.com>
2023-11-09 05:11:18 +0700

    Init angular client

--- api/Program.cs
+++ api/Program.cs
@@ -6,13 +6,19 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
-builder.Services.AddDbContext<DataContext>(options => {
+builder.Services.AddDbContext<DataContext>(options =>
+{
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+builder.Services.AddCors();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+app.UseCors(builder => builder.AllowAnyHeader()
+.AllowAnyMethod()
+.WithOrigins("http://localhost:4200"));
+
 app.MapControllers();
 
 app.Run();
