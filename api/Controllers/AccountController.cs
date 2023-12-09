c0f46bfbb5385fbdd7907434c4fb2883b9165dc3
Huy Tran Hoang <nhokpio95@gmail.com>
2023-12-09 20:16:24 +0700

    Add identity

--- api/Controllers/AccountController.cs
+++ api/Controllers/AccountController.cs
@@ -29,11 +29,7 @@ public class AccountController : BaseApiController
 
         var user = _mapper.Map<AppUser>(registerDto);
 
-        using var hmac = new HMACSHA512();
-
         user.UserName = registerDto.UserName.ToLower();
-        user.PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(registerDto.Password));
-        user.PasswordSalt = hmac.Key;
 
         _context.Users.Add(user);
         await _context.SaveChangesAsync();
@@ -55,15 +51,6 @@ public class AccountController : BaseApiController
             .SingleOrDefaultAsync(x => x.UserName == loginDto.UserName);
 
         if (user == null) return Unauthorized("Invalid username");
-
-        using var hmac = new HMACSHA512(user.PasswordSalt);
-
-        var computedHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(loginDto.Password));
-
-        for (int i = 0; i < computedHash.Length; i++)
-        {
-            if (computedHash[i] != user.PasswordHash[i]) return Unauthorized("Invalid password");
-        }
 
         return new UserDto
         {
